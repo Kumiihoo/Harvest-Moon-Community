18d75e82a6063d61a0bcdde85930f97f359cd681
Mario Medina <maralfonso.medina@gmail.com>
2022-05-08 05:19:07 +0200

    Toda la aplicación limpia a falta de lo de mis posts

--- app/views/posts/manage.php
+++ app/views/posts/manage.php
@@ -1,23 +1,23 @@
 <h1>Panel de Administración de Posts</h1>
 
-<a href="<?=base_url?>posts/create" class="button button-small">
+<a href="<?= base_url ?>posts/create" class="button button-small">
 	Nueva Entrada
 </a>
 
-<?php if(isset($_SESSION['producto']) && $_SESSION['producto'] == 'complete'): ?>
-	<strong class="alert_green">El producto se ha creado correctamente</strong>
-<?php elseif(isset($_SESSION['producto']) && $_SESSION['producto'] != 'complete'): ?>	
-	<strong class="alert_red">El producto NO se ha creado correctamente</strong>
+<?php if (isset($_SESSION['post']) && $_SESSION['post'] == 'complete') : ?>
+	<strong class="alert_green">El post se ha creado correctamente</strong>
+<?php elseif (isset($_SESSION['post']) && $_SESSION['post'] != 'complete') : ?>
+	<strong class="alert_red">El post NO se ha creado correctamente</strong>
 <?php endif; ?>
-<?php Utils::deleteSession('producto'); ?>
-	
-<?php if(isset($_SESSION['delete']) && $_SESSION['delete'] == 'complete'): ?>
-	<strong class="alert_green">El producto se ha borrado correctamente</strong>
-<?php elseif(isset($_SESSION['delete']) && $_SESSION['delete'] != 'complete'): ?>	
-	<strong class="alert_red">El producto NO se ha borrado correctamente</strong>
+<?php Utils::deleteSession('post'); ?>
+
+<?php if (isset($_SESSION['delete']) && $_SESSION['delete'] == 'complete') : ?>
+	<strong class="alert_green">El post se ha borrado correctamente</strong>
+<?php elseif (isset($_SESSION['delete']) && $_SESSION['delete'] != 'complete') : ?>
+	<strong class="alert_red">El post NO se ha borrado correctamente</strong>
 <?php endif; ?>
 <?php Utils::deleteSession('delete'); ?>
-	
+
 <table>
 	<tr>
 		<th>ID</th>
@@ -28,18 +28,18 @@
 		<th>Contenido del Post</th>
 		<th>ACCIONES</th>
 	</tr>
-	<?php while($pos = $post->fetch_object()): ?>
+	<?php while ($pos = $post->fetch_object()) : ?>
 		<tr>
-			<!-- <td><?=$pos->id;?></td> -->
-			<td><a href="<?= base_url ?>posts/detail&id=<?= $pos->id ?>"><?=$pos->id?></a></td>
-			<td><?=$cat_array[$pos->category_id];?></td>
-			<td><?=$user_array[$pos->author];?></td>
-			<td><?=$pos->title;?></td>
-			<td><?=$pos->date;?></td>
-			<td><?=$pos->content;?></td>
+			<!-- <td><?= $pos->id; ?></td> -->
+			<td><a href="<?= base_url ?>posts/detail&id=<?= $pos->id ?>"><?= $pos->id ?></a></td>
+			<td><?= $cat_array[$pos->category_id]; ?></td>
+			<td><?= $user_array[$pos->author]; ?></td>
+			<td><?= $pos->title; ?></td>
+			<td><?= $pos->date; ?></td>
+			<td><?= $pos->content; ?></td>
 			<td>
-				<a href="<?=base_url?>posts/editar&id=<?=$pos->id?>" class="button button-gestion">Editar</a>
-				<a href="<?=base_url?>posts/eliminar&id=<?=$pos->id?>" class="button button-gestion button-red">Eliminar</a>
+				<a href="<?= base_url ?>posts/editar&id=<?= $pos->id ?>" class="button button-gestion">Editar</a>
+				<a href="<?= base_url ?>posts/eliminar&id=<?= $pos->id ?>" class="button button-gestion button-red">Eliminar</a>
 			</td>
 		</tr>
 	<?php endwhile; ?>
